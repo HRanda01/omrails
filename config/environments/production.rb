9ba74ada6892ace2387ab73e22450a5b2042bfaf
H. Douglas Randall <drandall78@gmail.com>
2013-09-11 11:28:43 -0400

    12b Customizing Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Omrails::Application.configure do
+  
+  ::ActiveSupport::Deprecation.silenced = true
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
